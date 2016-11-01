3f0a1bbc66f70ed62cec01382dc7697c0395cdf9
Roma Kuzmin <romik-off-road@yandex.ru>
2016-11-01 10:52:20 +0300

    gps

--- _KOMPASS_KATAMARAN_V1.ino
+++ _KOMPASS_KATAMARAN_V1.ino
@@ -6,6 +6,14 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <TinyGPS++.h>
+#include <BMP085.h>
+#include <EEPROM.h>
+#include <EEPROMAnything.h>
+#include <Sunrise.h>
+#include <I2C_eeprom.h>
+#include <RTClib.h>
+#include <Average.h>
 
 #define OLED_MOSI   5
 #define OLED_CLK    7
@@ -15,30 +23,45 @@
 
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
-  long a;
-  long b;
-  float v;
-  
+long a;
+long b;
+float v;
+
+TinyGPSPlus gps;  // GPS Serial(1)4800
+char nmea;        // NMEA input
+
+// ====================== Setup ================================================
+
 void setup() {
 
   pinMode(23, OUTPUT);
   Serial1.begin(4800);  // GPS
   display.begin();
- 
+
 }
 
 void loop() {
-  
+
+  if (Serial1.available()) {
+    nmea = Serial1.read();
+    gps.encode(nmea);
+  }
+
   a = random(1,99);
   b = random(10,50);
-  
+
   digitalWrite(23, HIGH);   
-  delay(100);              
+ // delay(100);              
   digitalWrite(23, LOW);    
-  delay(100);         
-  
-  if (a > b) { v = a/b; } else { v = float(b/a); }
-  
+ // delay(100);         
+
+  if (a > b) { 
+    v = a/b; 
+  } 
+  else { 
+    v = float(b/a); 
+  }
+
   display.cp437(true); // Для русских букв  
   display.clearDisplay();
   display.setTextSize(2);
@@ -48,12 +71,12 @@ void loop() {
   display.println(utf8rus("Скорость:"));
   display.setTextColor(WHITE);
   display.setTextSize(4);
-  display.println(v);
+  display.println(gps.charsProcessed());
   display.setTextSize(2);
   display.println(utf8rus("км/ч"));
   display.display();
-  
-  
+
+
 }
 
 String utf8rus(String source)
@@ -61,31 +84,46 @@ String utf8rus(String source)
   int i,k;
   String target;
   unsigned char n;
-  char m[2] = { '0', '\0' };
+  char m[2] = { 
+    '0', '\0'   };
 
-  k = source.length(); i = 0;
+  k = source.length(); 
+  i = 0;
 
   while (i < k) {
-    n = source[i]; i++;
+    n = source[i]; 
+    i++;
 
     if (n >= 0xC0) {
       switch (n) {
-        case 0xD0: {
-          n = source[i]; i++;
-          if (n == 0x81) { n = 0xA8; break; }
+      case 0xD0: 
+        {
+          n = source[i]; 
+          i++;
+          if (n == 0x81) { 
+            n = 0xA8; 
+            break; 
+          }
           if (n >= 0x90 && n <= 0xBF) n = n + 0x30;
           break;
         }
-        case 0xD1: {
-          n = source[i]; i++;
-          if (n == 0x91) { n = 0xB8; break; }
+      case 0xD1: 
+        {
+          n = source[i]; 
+          i++;
+          if (n == 0x91) { 
+            n = 0xB8; 
+            break; 
+          }
           if (n >= 0x80 && n <= 0x8F) n = n + 0x70;
           break;
         }
       }
     }
-    m[0] = n; target = target + String(m);
+    m[0] = n; 
+    target = target + String(m);
   }
-return target;
+  return target;
 }
+
 
