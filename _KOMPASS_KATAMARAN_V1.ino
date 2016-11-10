90830384a5d96ef8d1e50befcd340cc0edaa7741
Roma Kuzmin <romik-off-road@yandex.ru>
2016-11-10 17:23:40 +0300

    graph

--- _KOMPASS_KATAMARAN_V1.ino
+++ _KOMPASS_KATAMARAN_V1.ino
@@ -17,8 +17,8 @@
 #include <EEPROMAnything.h>
 #include <Sunrise.h>
 #include <I2C_eeprom.h>
-#include <RTClib.h>
 #include <Average.h>
+#include <RTClib.h>
 #include <Rtc_Pcf8563.h>
 
 #define OLED_MOSI   5
@@ -27,149 +27,153 @@
 #define OLED_DC     1
 #define OLED_RESET  28
 
+unsigned long BAR_EEPROM_POS = 0;
 
-#define EEPROM_ADDRESS_256      (0x51) // 24LC256
-#define EEPROM_ADDRESS_32       (0x50) // EEPROM on RTC 
+#define EEPROM_ADDRESS      (0x50) // 24LC512
 
-#define BMP085_ADDRESS  0x77 // BMP085
-#define DS1307_ADDRESS  0x68 // DS1307
+#define EE24LC512MAXBYTES 524288/8 // 65536 Байт [0-655365]
 
-#define EE24LC32MAXBYTES   32768/8
-#define EE24LC256MAXBYTES 262144/8 // 32768 Байт [0-32767]
-
-I2C_eeprom  eeprom32(EEPROM_ADDRESS_32 ,EE24LC32MAXBYTES);
-I2C_eeprom eeprom256(EEPROM_ADDRESS_256,EE24LC256MAXBYTES);
+I2C_eeprom eeprom(EEPROM_ADDRESS,EE24LC512MAXBYTES);
 
 // ----------------------- BMP085 ---------------------------------
 
 struct bmp085_t // Данные о давлении,высоте и температуре
 {    
-    double Press,Alt,Temp;
-    unsigned long unix_time; 
-    
-} bmp085_data;
+  double Press,Alt,Temp;
+  unsigned long unix_time; 
+
+} 
+bmp085_data;
 
 struct bmp085_out // Данные о давлении,высоте и температуре
 {    
-    double Press,Alt,Temp;
-    unsigned long unix_time; 
-    
-} bmp085_data_out;
+  double Press,Alt,Temp;
+  unsigned long unix_time; 
+
+} 
+bmp085_data_out;
 
 
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
-long a;
-long b;
-float v;
-
-
 unsigned long currentMillis;
-unsigned long PreviousInterval = 0;     
-
+unsigned long PreviousInterval = 0;  
+unsigned long onePreviousInterval = 0; 
+unsigned long barPreviousInterval = 0;
 
 TinyGPSPlus gps;  // GPS Serial(1)4800
 char nmea;        // NMEA input
 
 Rtc_Pcf8563 rtc;  // PCF8563
 
-BMP085 bmp = BMP085();
+BMP085 bmp = BMP085();  // BMP085
 
 long Temperature = 0, Pressure = 0, Altitude = 0;
 
+#define FIVE_MINUT 300000
+#define TWO_DAYS 172800
+
+#define DEBUG 0
+
+#define LED 23
 
 // ====================== Setup ================================================
 
 void setup() {
 
-  pinMode(23, OUTPUT);  // LED
+  Serial.begin(9600);   // Debug
+  pinMode(LED, OUTPUT);  // LED
   Serial1.begin(4800);  // GPS
   display.begin();      // Turn On Display
   Wire.begin();
   delay(500);
+  bmp.init();  
 
   // rtc.initClock();  // Erase Clock
+
+  rtc.setSquareWave(SQW_1HZ);
+
+  // rtc.clearSquareWave();
   
-  // rtc.setSquareWave(SQW_1HZ);
+  // day, weekday, month, century(1=1900, 0=2000), year(0-99)
+  //  rtc.setDate(10, 4, 11, 0, 16);
+  // hr, min, sec
+  //  rtc.setTime(16,14,00);
   
-  rtc.clearSquareWave();
-  
-  bmp.init();  
-  
+    display.clearDisplay();
+    display.display();
 
 }
 
+// ================================ Main =====================================
+
+
 void loop() {
 
-  
-     
-   currentMillis = millis();
+   
+  currentMillis = millis();
 
-  if(currentMillis - updatePreviousInterval > 10000) {  // Каждые 10 секунд
-   updatePreviousInterval = currentMillis;  
+  if(currentMillis - PreviousInterval > (FIVE_MINUT*3) ) {  // 15 Минут
+    PreviousInterval = currentMillis;  
 
-   dps.getTemperature(&Temperature);  // Температура
-   dps.getPressure(&Pressure);        // Давление
-   dps.getAltitude(&Altitude);        // Высота 
+    bmp.getTemperature(&Temperature);  // Температура
+    bmp.getPressure(&Pressure);        // Давление
+    bmp.getAltitude(&Altitude);        // Высота 
 
-   sensors.requestTemperatures(); 
-   tempC = sensors.getTempC(EXT_Thermometer);
+    bmp085_data.Press = Pressure/133.3;
+    bmp085_data.Alt   = Altitude/100.0;
+    bmp085_data.Temp  = Temperature/10.0;
 
-   bmp085_data.Press = ( bmp085_data.Press + Pressure/133.3 ) / 2.0;
-   bmp085_data.Alt   = ( bmp085_data.Alt + Altitude/100.0 ) / 2.0;
-   bmp085_data.Temp  = ( bmp085_data.Temp + tempC ) / 2.0;
+    Save_Bar_Data();
 
   }
 
 
-
-  
-   bmp085_data.Press = Pressure/133.3;
-  bmp085_data.Alt   = Altitude/100.0;
-  bmp085_data.Temp  = tempC;
-  
-  
   if (Serial1.available()) {
     nmea = Serial1.read();
     gps.encode(nmea);
   }
 
-  a = random(1,99);
-  b = random(10,50);
-
-  digitalWrite(23, HIGH);   
-  // delay(100);              
-  digitalWrite(23, LOW);    
-  // delay(100);         
-
-  if (a > b) { 
-    v = a/b; 
-  } 
-  else { 
-    v = float(b/a); 
+  if(currentMillis - onePreviousInterval > 1000 ) {  // 1 Секунда
+    onePreviousInterval = currentMillis;  
+    Display_Test();
+    if (digitalRead(LED) == 1) digitalWrite(LED,LOW); 
+    else digitalWrite(LED,HIGH);
   }
 
-  bmp.getTemperature(&Temperature); 
-  bmp.getPressure(&Pressure);
-  bmp.getAltitude(&Altitude);
-  
+}
+
+// =============================== Functions ================================
+
+
+void Display_Test() {
+
   display.cp437(true); // Для русских букв  
-  display.clearDisplay();
+  //display.clearDisplay();
   display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setTextWrap(0);
+  display.fillRect(0,0,127,16,BLACK);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.setTextWrap(0);
-  // display.println(utf8rus("Скорость:"));
-  display.print(Temperature/10.0);
-  display.print(" ");
-  display.println(Pressure/133.3);
-  
-  display.setTextColor(WHITE);
-  display.setTextSize(4);
-  display.println(gps.charsProcessed());
-  display.setTextSize(2);
-//  display.println(utf8rus("км/ч"));
   display.print(rtc.formatTime());
+
+  //  display.println(utf8rus("Скорость:"));
+  //  display.print(" ");
+  //  display.println(Pressure/133.3);
+  //  display.setTextColor(WHITE);
+  //  display.setTextSize(4);
+  //  display.println(gps.charsProcessed());
+  //  display.setTextSize(2);
+  //  display.println(utf8rus("км/ч"));
+  //  display.print(rtc.formatTime());
+  //  virtual void drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
+  //  virtual void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
+
+  //  virtual void drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
+  //  virtual void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
+
+  ShowBMP085();
   display.display();
 
 }
@@ -180,7 +184,7 @@ String utf8rus(String source)
   String target;
   unsigned char n;
   char m[2] = { 
-    '0', '\0'         };
+    '0', '\0'                       };
 
   k = source.length(); 
   i = 0;
@@ -244,52 +248,127 @@ void Turn_On_MPU( void ) {
 
 }
 
-
 // --------------------------- Save Barometer Data to EEPROM -------------------------------------
 
 void Save_Bar_Data( void ) {
 
-    // 48 часов * 60 минут = 2880 Минут
-    // 2880 минут / 30 минут = 96 Ячеек
-    // (UnixTime / 1800) % 96 = номер ячейки
+  // 48 часов * 60 минут = 2880 Минут
+  // 2880 минут / 30 минут = 96 Ячеек
+  // (UnixTime / 1800) % 96 = номер ячейки
 
-   dps.getTemperature(&Temperature);  // Температура
-   dps.getPressure(&Pressure);        // Давление
-   dps.getAltitude(&Altitude);        // Высота 
-   
-   DateTime now = rtc.now();
-  
-   bmp085_data.unix_time = now.unixtime(); 
-   
-   // DEBUG
-   
-   if (DEBUG) {
-     bt.print("h:"); bt.println(now.hour());
-     bt.print("m:"); bt.println(now.minute());
-     bt.print("p:"); bt.println((bmp085_data.unix_time/1800)%96);
-   }
-   
-   BAR_EEPROM_POS = ( (bmp085_data.unix_time/1800)%96 ) * sizeof(bmp085_data); // Номер ячейки памяти.
-  
-   sensors.requestTemperatures(); 
-   tempC = sensors.getTempC(EXT_Thermometer);  
-       
-   bmp085_data.Press = ( bmp085_data.Press + Pressure/133.3 ) / 2.0;
-   bmp085_data.Alt   = ( bmp085_data.Alt + Altitude/100.0 ) / 2.0;
-   bmp085_data.Temp  = ( bmp085_data.Temp + tempC ) / 2.0;
-     
-   if (DEBUG) {
-    bt.println(BAR_EEPROM_POS/sizeof(bmp085_data));
-    bt.println(bmp085_data.unix_time);
-    bt.println(now.hour());
-    bt.println(now.minute());   
-    bt.println("-------");
-   }
-   
-   const byte* p = (const byte*)(const void*)&bmp085_data;
-   for (unsigned int i = 0; i < sizeof(bmp085_data); i++) 
-    eeprom32.writeByte(BAR_EEPROM_POS++,*p++);
-   
+  bmp.getTemperature(&Temperature);  // Температура
+  bmp.getPressure(&Pressure);        // Давление
+  bmp.getAltitude(&Altitude);        // Высота 
+
+  DateTime now = DateTime (rtc.getYear(), 
+  rtc.getMonth(), 
+  rtc.getDay(),
+  rtc.getHour(), 
+  rtc.getMinute(), 
+  rtc.getSecond());
+
+  bmp085_data.unix_time = now.unixtime(); 
+
+  if (DEBUG) {
+    Serial.println(bmp085_data.unix_time);
+  }
+
+  BAR_EEPROM_POS = ( (bmp085_data.unix_time/1800)%96 ) * sizeof(bmp085_data); // Номер ячейки памяти.
+
+  bmp085_data.Press = Pressure/133.3;
+  bmp085_data.Alt   = Altitude/100.0;
+  bmp085_data.Temp  = Temperature/10.0;
+
+  const byte* p = (const byte*)(const void*)&bmp085_data;
+  for (unsigned int i = 0; i < sizeof(bmp085_data); i++) 
+    eeprom.writeByte(BAR_EEPROM_POS++,*p++);
+
 }
+
+
+// ---------------- Barometer Graphics ------------------------
+
+void ShowBMP085() {
+
+  int x;
+
+   display.drawFastVLine(0,20,43, WHITE);
+   display.drawFastHLine(0,63,127, WHITE);
+    
+  if (currentMillis - barPreviousInterval > FIVE_MINUT/2) {  
+    barPreviousInterval = currentMillis;      
+
+    DateTime now = DateTime (rtc.getYear(), 
+    rtc.getMonth(), 
+    rtc.getDay(),
+    rtc.getHour(), 
+    rtc.getMinute(), 
+    rtc.getSecond());  
+
+    byte current_position = (now.unixtime()/1800)%96;  
+
+    Average<double> bar_data(96); // Вычисление максимального и минимального значения
+
+    double barArray[96];   
+
+    BAR_EEPROM_POS = 0;
+
+    for(byte j = 0;j < 96; j++) {           
+
+      byte* pp = (byte*)(void*)&bmp085_data_out; 
+
+      for (unsigned int i = 0; i < sizeof(bmp085_data); i++)
+        *pp++ = eeprom.readByte(BAR_EEPROM_POS++); 
+
+      if ((now.unixtime() - bmp085_data.unix_time) < TWO_DAYS) {
+
+        barArray[j] = bmp085_data.Press; 
+        bar_data.push(bmp085_data.Press);
+
+      } 
+      else {
+
+        barArray[j] = 0.0;
+
+      }
+      
+    }
+
+    BAR_EEPROM_POS = 0;
+
+    int y_pres = 127;
+
+    for(byte j=0;j<96;j++) {
+
+      if (j != 0) {
+        x = map(barArray[current_position],bar_data.minimum()-1,bar_data.maximum()+1,43,1);
+      } 
+      else {
+        x = map(Pressure,bar_data.minimum()-1,bar_data.maximum()+1,43,1); // Текущие значение
+      }
+
+      display.drawLine(0,y_pres,42,y_pres, BLACK); // Стереть линию
+
+      if (barArray[current_position] != 0.0) {     
+       display.drawLine(x,y_pres,42,y_pres, WHITE); // Нарисовать данные    
+      }
+
+      if (current_position == 0) current_position = 96;
+
+      current_position--; 
+
+      y_pres--;
+
+    } 
+
+  }  
+
+}
+
+
+
+
+
+
 
 
